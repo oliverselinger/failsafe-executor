4b176a38e20e67c02723a866c6778e80ff0961e9
Miruna-Diana Jarda <miruna_jarda_diana@yahoo.com>
2023-05-09 08:16:31 +0200

    Add nodeId to tests

--- src/test/java/os/failsafe/executor/TaskShould.java
+++ src/test/java/os/failsafe/executor/TaskShould.java
@@ -11,14 +11,14 @@ class TaskShould {
 
     @Test
     void return_false_on_cancel_if_task_is_not_cancelable_because_it_is_locked() {
-        Task task = new Task("id", "name", "parameter", LocalDateTime.now(), LocalDateTime.now(), LocalDateTime.now(), null, 0, 0L);
+        Task task = new Task("id", "name", "parameter", "nodeId", LocalDateTime.now(), LocalDateTime.now(), LocalDateTime.now(), null, 0, 0L);
 
         assertFalse(task.isCancelable());
     }
 
     @Test
     void return_false_on_retry_if_task_is_not_retryable() {
-        Task task = new Task("id", "name", "parameter", LocalDateTime.now(), LocalDateTime.now(), null, null, 0, 0L);
+        Task task = new Task("id", "name", "parameter", "nodeId", LocalDateTime.now(), LocalDateTime.now(), null, null, 0, 0L);
 
         assertFalse(task.isRetryable());
     }
@@ -27,8 +27,7 @@ class TaskShould {
     void print_its_internal_state() {
         LocalDateTime dateTime = LocalDateTime.of(2020, 5, 5, 10, 30);
         Task task = new Task("id", "name", "parameter", dateTime, dateTime, dateTime, new ExecutionFailure(dateTime, "exceptionMsg", "stackTrace"), 0, 0L);
-
-        assertEquals("Task{id='id', parameter='parameter', name='name', creationTime=2020-05-05T10:30, plannedExecutionTime=2020-05-05T10:30, lockTime=2020-05-05T10:30, executionFailure=ExecutionFailure{failTime=2020-05-05T10:30, exceptionMessage='exceptionMsg', stackTrace='stackTrace'}, version=0}",
+        assertEquals("Task{id='id', parameter='parameter', name='name', nodeId='null', creationTime=2020-05-05T10:30, plannedExecutionTime=2020-05-05T10:30, lockTime=2020-05-05T10:30, executionFailure=ExecutionFailure{failTime=2020-05-05T10:30, exceptionMessage='exceptionMsg', stackTrace='stackTrace'}, version=0}",
                 task.toString());
     }
 
